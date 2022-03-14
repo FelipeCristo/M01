0175a06893694c914e2f1d425f02498c8777536a
Felipe <felipe.cristo@yahoo.com.br>
2022-03-14 20:53:12 -0300

    alteração

--- Projeto1/projeto01.js
+++ Projeto1/projeto01.js
@@ -8,7 +8,7 @@ let pontos = 0;
 
 
 console.log("\n\t Após anos de guerra declarada ao seu reino, o país vizinho já está com suas forças cansadas e com a moral baixa depois de tantas derrotas nos campos de batalha. Você que no início de sua empreitada era apenas um escudeiro e hoje foi condecorado como cavaleiro depois de sua demonstração de coragem e bravura no campo de batalha. Sua intimidade com o uso da espada logo tinha-se espalhado pelo seu reino e também pelo país vizinho, seus inimigos, onde você ficou conhecido como Víbora, por causa da sua velocidade com a espada que os fazia lembrar de uma cobra dando um bote. O Rei inimigo jurou que iria pagar cada baixa que você causou em seu exército, por ter frustrado suas tentativas te expansão de seu território, mas principalmente por ter ceifado a vida de seu único filho, que comandava um destacamento que tinha caído em uma emboscada que você tinha planejado junto com seus superiores na época. Ele sabia que a ideia toda foi sua, e que apenas foi aprovada pelos oficias. O Rei inimigo, tamanha sua dor, de perda e de orgulho ferido e por ódio a ti, contratou assassinos mercenários para fazer você pagar por tudo que tinha feito a ele, dizendo que quando menos esperasse você morreria. \n\t Hoje é dia de comemoração e nosso Rei fez uma festa em sua homenagem devida a sua condecoração no castelo.\n\t Belas moças serviam comida e bebidas a todos no grande salão, trovadores e menestréis tocavam uma música animada que ecoavam por todos os cantos do ambiente e todos pareciam muito alegres e satisfeitos com tudo. \n\t Uma das moças que estava servindo vinho se aproxima de ti. Vestindo um vestido claramente gasto, mas bem cuidado, e de cor amarela. Ela era muito bonita, com sua pele morena e seu cabelo cacheado. E lhe ofereceu vinhos com especiarias. Mas como você foi criado desde que nasceu ao redor do castelo, você percebe que nunca tinha visto essa moça antes. Ela uma estranha para ti. \t\n ");
-resposta = prompt ('Você recusa o vinho?\t').toLowerCase();
+resposta = prompt ('Você recusa o vinho?').toLowerCase();
     if (resposta == 'sim'){
         console.log('você apenas continuar curtindo a festa, mas fica de olho nela');
         if (resposta == "sim") {
@@ -19,7 +19,7 @@ resposta = prompt ('Você recusa o vinho?\t').toLowerCase();
 }
 console.log('\n\t A festa continua e todos estão bebendo, comendo e dançando. Chegado uma hora estoura uma briga ao seu lado e dois Caras enormes se digladiam. Claramente ambos estão bêbados. E toda a festa presta atenção neles.\n');
 
-resposta = prompt ('Você tenta apartar a briga?\t').toLowerCase();
+resposta = prompt ('Você tenta apartar a briga?').toLowerCase();
     if (resposta == 'sim'){
         console.log('você apenas continuar curtindo a festa, mas fica de olho nela');
         if (resposta == "sim") {
@@ -31,7 +31,7 @@ resposta = prompt ('Você tenta apartar a briga?\t').toLowerCase();
 
 console.log('\n\tA paz volta ao recinto logo após a briga ser separada e os dois brigões são levados para uma cela na prisão do castelo para passar a noite. O clima no grande Salão está bem animado e o povo entra em euforia assim que entra um grupo de malabaristas e outros artistas. Um cuspia fogo, outro engolia espadas, e um outro fazia malabares com espadas e facas jogando-as para cima e as pegando com precisão quando chegava em baixo formando um círculo perfeito de objetos mortais. No meio das apresentações uma movimentação lhe chamou atenção próximo a porta de entrada do salão. Era uma linda mulher que cresceu na cidade próximo ao castelo e que você era apaixonado desde sua adolescência. Filha do banqueiro da cidade, até então intocada para um mero escudeiro. Mas hoje tudo mudou. Com a sua promoção para cavaleiro o pai dela aceitou seu noivado com ela. Assim que ela lhe viu, ela sorriu e foi a seu encontro. Mas o açougueiro não estava satisfeito com essa notícia do seu noivado. Ele também a amava e preferia vê-la morta do que com outro homem. Ele trouxe uma balestra escondida sob sua capa. Aproveitando que todos estavam entretidos com o espetáculo, ele mirou a flecha bem no coração de sua noiva. \n\t No segundo antes do gatilho ser puxado você viu de relance o brilho de metal, da ponta da flecha que estava sendo apontado para a direção dela\n');
 
-resposta = prompt ('Você tenta se jogar em direção da flecha para tentar salva-la\t').toLowerCase();
+resposta = prompt ('Você tenta se jogar em direção da flecha para tentar salva-la').toLowerCase();
     if (resposta == 'sim'){
         console.log('você puxa a para trás de você e ele erra o disparo');
         if (resposta == "sim") {
@@ -44,7 +44,7 @@ resposta = prompt ('Você tenta se jogar em direção da flecha para tentar salv
 console.log('\n\tVocê se certifica que ela está fora de perigo e a deixa aos cuidados de seus homens de confiança e de sua família, partindo com tudo para cima do açougueiro. Que cai de costas desesperado, deixando ao chão sua balestra ao se levantar correndo e pondo-se para fora do salão atropelando todos em seu caminho antes que percebessem o que tinha acontecido ali. Você o perseguiu até o pátio do castelo. Chegando, reparou que tinha começado a nevar a pouco tempo, o chão estava úmido escorregadio e enlameado. O ar estava gélido, e uma nuvem de fumaça saia de sua respiração ofegante após a longa corrida pelo castelo. O Agressor de sua amada estava lhe encarando com cara nítida de escárnio. Atrás dele havia os portões em grade de aço largo trancado, que dava acesso a cidade. Obviamente ele não esperava que o portão estivesse fechado a essa hora. Ele segurava uma espada de modo torto e de forma clara que não havia recebido treino algum de como manusear uma espada, muito menos como segura-la. Ela era velha e mau cuidada. Parece uma espada de treino, que ele provavelmente deve ter achado ali por perto. E com ar de deboche ele disse:\n\t  - O Grande herói veio tirar um pedaço de carne do açougueiro? - e riu como um louco\n Em meio as sombras que se formava ao lado dos muros, próximo de um barracão de ferramentas que se encontrava ali, surgiu a moça que havia lhe oferecido o vinho mais cedo no grande salão. Mas agora ela estava vestida com uma armadura acolchoada de couro e lã bem escuros, quase preto, e botas pesadas. Em suas mãos haviam duas adagas. De susto o açougueiro soltou um grunhido de porco. E antes que ele pudesse latir alguma coisa para ela. Uma das adagas foi parar no meio de seus olhos. Mesmo que uns bons 5 metros os separassem. Ela o olhou de cima para baixo e logo retirou outra adaga de seu cinto. \n\t Caminhou calmamente até o corpo gordo e morto do açougueiro e retirou da adaga. A limpou no gibão do açougueiro e guardou em seu cinto onde a outra estava. Pegou a espada velha e a jogou para ti. Claramente querendo que você a apanhasse.\n\t Agora ambos estavam armados e se encarando em meio a noite bem iluminada pela lua e com uma neve caindo levemente sobre vocês. Você não sabe quem é essa moça, mas sabe que ela não está para brincadeiras, muito menos para fazer amizades. Só estão vocês dois em meio ao pátio gelado com uma música do grande salão rondando ao fundo.Ao que parece ninguém vem para ver o que está acontecendo ou até');
 console.log('se alguém sabe o que aconteceu dentro do salão. Mas parece que a festa continua sem nenhuma preocupação. Não há sinal algum de guardas próximos deste lugar.\n');
 
-resposta = prompt ('Gritar por ajudar?\t').toLowerCase();
+resposta = prompt ('Gritar por ajudar?').toLowerCase();
     if (resposta == 'sim'){
         console.log('Os cães começam a latir, mas parece que ninguém além deles escudou você');
         if (resposta == "sim") {
@@ -58,9 +58,9 @@ resposta = prompt ('Gritar por ajudar?\t').toLowerCase();
 console.log('\tEla começa o ataque ferozmente. sua forma de lutar com duas adagas são formidáveis. Você nunca tinha visto isso antes. Era rápido e gracioso, quase como se ela estivesse dançando com você. Te conduzindo, fazendo você seguir o ritmo dela. Até que uma de suas adagas se enterrou em sua coxa esquerda. E você solta do grito de dor, mas não se abala.  Se aproveita que ela baixou a guarda próximo de ti, aparentemente ela achou que já havia ganho a batalhar por ter te acertado primeiro. Você a agarra abruptamente pela gola de sua armadura e a arremessa contra o chão. Que a faz perder completamente o ar de seus pulmões.\n ');
 
 
-resposta = prompt ('Colocando sua espada sobre a garganta dela, você quer interroga-la?\t').toLowerCase();
+resposta = prompt ('Colocando sua espada sobre a garganta dela, você quer interroga-la?').toLowerCase();
     if (resposta == 'sim'){
-        console.log('\tEla conta que é uma latina do reino inimigo, mas que pegaram sua família de refém e a obrigou a fazer isso para ajudar na vingando de seu Rei. Caso contrario iriam matar todos que ela um dia já amou. Ela prometeu ajudar a derrubar o Rei inimigo, já que conhece suas fraquezas.');
+        console.log('Ela conta que é uma latina do reino inimigo, mas que pegaram sua família de refém e a obrigou a fazer isso para ajudar na vingando de seu Rei. Caso contrario iriam matar todos que ela um dia já amou. Ela prometeu ajudar a derrubar o Rei inimigo, já que conhece suas fraquezas.');
         if (resposta == "sim") {
             pontos++ }
     } else if(resposta == 'nao');{
@@ -68,7 +68,7 @@ resposta = prompt ('Colocando sua espada sobre a garganta dela, você quer inter
     
     }   
 
-console.log('Pelos seus méritos, em sua empreitada...')
+console.log('Pelos seus méritos, em sua empreitada...\n')
 
 if (pontos == 0) {
     console.log('Você falha miserávelmente');
